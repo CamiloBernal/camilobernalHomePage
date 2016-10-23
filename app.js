e2a5214450e56b03145ec06a30f810d25d46b7ef
Camilo Bernal <angelcamilob@gmail.com>
2016-10-23 13:42:39 -0500

    Add Azure Application Insights

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
 var express = require('express'),
   config = require('./config/config'),
   glob = require('glob'),
-  mongoose = require('mongoose');
+  mongoose = require('mongoose'),
+  appInsights = require("applicationinsights"),
+  azureInsightsInstrumentationKey = "846224f4-ddba-4004-9278-ef8da7fa5f16";
+
 
 mongoose.connect(config.db);
 var db = mongoose.connection;
@@ -18,6 +21,8 @@ models.forEach(function (model) {
 var app = express();
 
 require('./config/express')(app, config);
+
+appInsights.setup(azureInsightsInstrumentationKey).start();
 
 app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
